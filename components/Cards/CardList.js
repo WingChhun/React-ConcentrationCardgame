0d026e1e8f167ee9f242528ebf283a6a07e78b8c
WingChhun <capt.wing.chhun@gmail.com>
2018-06-04 20:44:57 -0700

    Add cardlsit

--- components/Cards/CardList.js
+++ components/Cards/CardList.js
@@ -1,12 +1,27 @@
 import React, {Component} from 'react';
+import propTypes from 'prop-types';
 
-const CardList = () => {
+//*Import components
+import Card from "./Card";
+
+//*API URL for reference
+const API_DRAW_CARDS = "https://deckofcardsapi.com/api/deck/<<deck_id>>/draw/?count=52";
+const API_BASE_URL = "https://deckofcardsapi.com/api/deck/";
+/*
+ TODO: CardList
+*/
+
+const CardList = ({deck, card}) => {
     return (
-        <div>
-            <h1>
-                CardList Component</h1>
-        </div>
-    )
-}
+        <h1>CardList CCOmponent</h1>
 
+    );
+}
 export default CardList;
+
+//! - Proptype check
+CardList.propTypes = {
+
+    deck: propTypes.shape({deck_id: propTypes.string, remaining: propTypes.number, shuffled: propTypes.bool, success: propTypes.bool}),
+    cards: propTypes.array.isRequired
+};
