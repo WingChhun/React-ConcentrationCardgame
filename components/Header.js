5de8d6eb8021bebb4af69e09e17409284dd95995
WingChhun <capt.wing.chhun@gmail.com>
2018-06-05 13:33:11 -0700

    Deleted

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,23 @@
 import React, {Component} from 'react';
-
-const Header = () => {
+import propTypes from 'prop-types';
+const Header = ({
+    cardPile = null,
+    _shuffleDeck
+}) => {
     return (
-        <div>
-            <h1>
-                Header Component</h1>
+        <div>   
+         <h1 >New Game</h1>
+      
+        
         </div>
     )
 }
 
 export default Header;
+
+//! PropTypes check
+Header.propTypes = {
+    cardPile: propTypes.array.isRequired,
+    _shuffleDeck: propTypes.func.isRequired
+
+};
