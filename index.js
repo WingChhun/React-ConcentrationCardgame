f7801315ee086ff4d66ead2bf23b0e32e25716e2
WingChhun <capt.wing.chhun@gmail.com>
2018-06-05 20:48:02 -0700

    Add font awesome

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//! Import css and font-awesome
 import './index.css';
+import "./node_modules/font-awesome/css/font-awesome.min.css";
+
+//! Import Components
 import App from './App';
 import Router from "./container/Router";
 import registerServiceWorker from './registerServiceWorker';
