e0f334a80959ed9af97e0dd9fd2a0a87f2e21fe7
WingChhun <capt.wing.chhun@gmail.com>
2018-06-05 20:48:12 -0700

    Import footer component from previous project

--- components/Footer.js
+++ components/Footer.js
@@ -1,12 +1,24 @@
-import React, {Component} from 'react';
+import React from 'react';
 
 const Footer = () => {
+
     return (
-        <div>
-            <h1>
-                Footer Component</h1>
-        </div>
+
+        <footer className="footer">
+
+            <ul className="footer__list">
+                <li className="footer__list--item"><a href="" className="fa fa-2x fa-github-square"/></li>
+                <li className="footer__list--item"><a href="" className="fa fa-2x fa-instagram"/></li>
+                <li className="footer__list--item"><a href="" className="fa fa-2x fa-linkedin-square"/></li>
+                <li className="footer__list--item"><a href="" className="fa fa-2x fa-codepen"/></li>
+            </ul>
+            {/*Copyright*/}
+            <p className="footer__copyright">&copy; James Chhun 2018
+            </p>
+        </footer>
+
     )
+
 }
 
 export default Footer;
